0bf50251d22f6ba7c588103924c5af15e41d08f6
Sai Dinesh Kopparthi <kopparthi.dinesh42224@gmail.com>
2025-02-10 21:24:49 -0600

    Episode Three - Part Two and Three

--- Chapter 01/Hands On/App.js
+++ Chapter 01/Hands On/App.js
@@ -1,36 +1,14 @@
 import React from "react";
-//import ReactDOM from "react-dom"; Not going to work after React 18
-import { createRoot } from 'react-dom/client';
+import ReactDOM from "react-dom/client";
 
 const heading = React.createElement(
   "h1",
   { id: "heading" },
-  "Hello World From React!"
+  "Hola! Buenos Dias."
 );
-{
-  /* <div id="parent">
-    <div id="child">
-      <h1>Creating nested HTML elements using React</h1>
-      <h2>Second element</h2>
-    </div>
-    <div id="child2">
-      <h1>Creating nested HTML elements using React</h1>
-      <h2>Second element</h2>
-    </div>
-  </div>; */
-}
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "Creating nested HTML elements using React"),
-    React.createElement("h2", {}, "Second element"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "Creating nested HTML elements using React"),
-    React.createElement("h2", {}, "Second element"),
-  ]),
-]);
+const jsxHeading = <h1 id="heading">Hola! Buenos Dias.</h1>;
 
-//const root = ReactDOM.createRoot(document.getElementById("root")); //Old way of rendering - before react 18
-const root = createRoot(document.getElementById('root'));
-root.render(parent);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(jsxHeading);
