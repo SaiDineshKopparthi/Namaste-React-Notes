8bf11481b7d002b8eec58ab0b8ee2c2ce889297b
Sai Dinesh Kopparthi <kopparthi.dinesh42224@gmail.com>
2025-02-26 18:09:41 -0600

    Episode Four - Part One

--- Chapter 01/Swiggy/App.js
+++ Chapter 01/Swiggy/App.js
@@ -1,3 +1,75 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+const Header = () => {
+  return (
+    <div className="header">
+      <div>
+        <img
+          className="logo"
+          src="https://marketplace.canva.com/EAGXsRURT9o/1/0/1200w/canva-yellow-and-brown-kitchen-food-logo-DRT6KFcL0ts.png"
+          alt="The logo of the food delivery app"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const resCardBackground = {
+  backgroundColor: "#f0f0f0",
+};
+
+const RestaurantCard = () => {
+  return (
+    <div className="res-card" style={resCardBackground}>
+      <img src="https://yummyindiankitchen.com/wp-content/uploads/2021/05/chicken-dum-biryani-hyderabadi-style.jpg" alt="" />
+      <h3>Kasif Dum Biryani</h3>
+      <h4>Andhra, Chinese, North Indian, Italian</h4>
+      <h4>4.9</h4>
+      <h4>15 min</h4>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div id="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<AppLayout />);
