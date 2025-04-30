f94097a254e9753b2903196ea10809516b3597dd
Sai Dinesh Kopparthi <kopparthi.dinesh42224@gmail.com>
2025-04-29 21:04:29 -0500

    Episode Eight - Part Two

--- Chapter 01/Swiggy/src/components/UserClass.js
+++ Chapter 01/Swiggy/src/components/UserClass.js
@@ -3,15 +3,21 @@ import React from "react";
 class UserClass extends React.Component {
   constructor(props) {
     super();
-    console.log(this.props);
+    console.log(props);
+    this.state = {
+      count: 10,
+      occupation: "SDE",
+    };
   }
 
   render() {
     return (
       <div className="user-card">
-        <h2>Name: {this.props.name}</h2>
+        <h2>Name: {name}</h2>
         <h3>Location: Missouri</h3>
         <h4>Contact: @dinesh</h4>
+        <h4>Occupation: {this.state.occupation}</h4>
+        <h4>Has ordered {this.state.count} times</h4>
       </div>
     );
   }
